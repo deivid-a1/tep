3335f960658361830dbe113af5dde66f4e7b1ac5
Deivid Carvalho <deivid_506_02@hotmail.com>
2022-11-20 05:57:40 -0300

    args

---
+++ aula/delimitador.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+
+
+bool is_valid(cons string& expr)
+{
+	std::stack<char> s;
+
+	for(auto c: expr)
+	{
+		switch(c) {
+			case 'c';
+			case '[';
+			...
+		}
+	}
+
+	return "incompleta";
+}	
+
+int main()
+{
+
+	
+
+	return 0;
+}
